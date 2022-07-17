b2f00b6f6196ea23184830b4d5755485a70dfb8a
georgiy <lovemoma1337@gmail.com>
2022-07-17 14:22:50 +0300

    пробное v2

--- neuro.py
+++ neuro.py
@@ -3,10 +3,11 @@ DEFAULT_ALPHA = 0.01
 
 class Neuro:
     def __init__(self, in_put: list, out_put: list, alpha: float = None):
-        self.in_put = in_put
+        self.in_put = in_put if isinstance(in_put[0], list) else [in_put]
         self.out_put = out_put
         self.alpha = alpha or DEFAULT_ALPHA
-        self.weight = [[1 for i in range(len(in_put))] for j in range(len(out_put))]
+        self.weight = [[1 for i in range(len(self.in_put[0]))] for j in range(len(out_put))]
+
         self.pred = None
         self.error = None
         self.delta = None
@@ -14,27 +15,27 @@ class Neuro:
 
     def calc(self, count_iter: int = 10000):
         for iteration in range(count_iter):
+            for data in self.in_put:
+                self.pred = self.neuro_network(data)
+                self.error = [0 for i in range(len(data))]
+                self.delta = [0 for i in range(len(data))]
+                self.weight_delta = [[1 for i in range(len(data))] for j in range(len(self.out_put))]
 
-            self.pred = self.neuro_network(self.in_put, self.weight)
-            self.error = [0 for i in range(len(self.in_put))]
-            self.delta = [0 for i in range(len(self.in_put))]
-            self.weight_delta = [[1 for i in range(len(self.in_put))] for j in range(len(self.out_put))]
+                for i in range(len(self.out_put)):
+                    self.delta[i] = self.pred[i] - self.out_put[i]
+                    self.error[i] = self.delta[i] ** 2
+                    for j in range(len(self.delta)):
+                        self.weight_delta[i][j] = data[j] * self.delta[i]
 
-            for i in range(len(self.out_put)):
-                self.delta[i] = self.pred[i] - self.out_put[i]
-                self.error[i] = self.delta[i] ** 2
-                for j in range(len(self.delta)):
-                    self.weight_delta[i][j] = self.in_put[j] * self.delta[i]
+                for i in range(len(self.weight)):
+                    for j in range(len(self.weight[0])):
+                        self.weight[i][j] -= self.weight_delta[i][j] * self.alpha
 
-            for i in range(len(self.weight)):
-                for j in range(len(self.weight[0])):
-                    self.weight[i][j] -= self.weight_delta[i][j] * self.alpha
-
-    def neuro_network(self, inp, weight):
+    def neuro_network(self, inp):
         output = [0 for i in range(len(self.out_put))]
 
-        for i in range(len(weight)):
-            output[i] = self.w_sum(inp, weight[i])
+        for i in range(len(self.weight)):
+            output[i] = self.w_sum(inp, self.weight[i])
 
         return output
 
--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 from neuro import Neuro
 
-inp = [8.4, 0.24, 1.1]
-goal_pred = [1]
+inp = [[1, 0, 0],
+       [0, 1, 0],
+       [0, 0, 1]]
+goal_pred = [0, 0, 1]
 
 neuro = Neuro(inp, goal_pred)
-neuro.calc(5)
-print(neuro)
+neuro.calc(50)
+import random
+for _ in range(10):
+    cvet = random.randint(0, 2)
+    svetofor = [0, 0, 0]
+    svetofor[cvet] = 1
+    print(neuro.neuro_network(svetofor))
